17cddc3f822d22a4e45098f9af251a9e753468c7
Arif Hossain <arifthecoder@gmail.com>
2018-10-18 23:40:53 +0300

    TextFieldGroup component was created

---
+++ client/src/components/common/TextFieldGroup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classnames from 'classnames';
+import propTypes from 'prop-types';
+
+const TextFieldGroup = ({
+    name,
+    placeholder,
+    value,
+    label,
+    error,
+    info,
+    type,
+    onChange,
+    disabled
+}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default TextFieldGroup;
